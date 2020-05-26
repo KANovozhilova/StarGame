c2a4897e87f33b2ea4e54542345a477a1721df00
novozhilova <nowozhilowa-ksenia@mail.ru>
2020-05-26 20:19:02 +0300

    Homework3

---
+++ core/src/com/geekbrains/sprite/Background.java
@@ -0,0 +1,19 @@
+package com.geekbrains.sprite;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.geekbrains.base.Sprite;
+import com.geekbrains.math.Rect;
+
+public class Background extends Sprite {
+
+    public Background(Texture texture) {
+        super(new TextureRegion(texture));
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        setHeightProportion(1f);
+        this.pos.set(worldBounds.pos);
+    }
+}
